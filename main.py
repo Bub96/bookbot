7d120227187a44a87675becb272edf050946cc6d
Bub96 <loganeck197@gmail.com>
2024-08-19 22:20:45 -0500

    added a function to print out the number of times a letter was used

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
     book_path = 'books/frankenstein.txt'
     txt = get_book_text(book_path)
-    print(txt)
+    print(num_char(txt))
     print(num_of_words(txt))
 
 def get_book_text(path):
@@ -11,7 +11,14 @@ def get_book_text(path):
 def num_of_words(book):
     return (len(book))
 
-
-
+def num_char(book):
+    letters={}
+    text = book.lower()
+    for t in text:
+        if t in letters:
+            letters[t] += 1
+        else:
+            letters[t] = 1
+    return letters
 
 main()
