71bdad49992f0b098ae11651eb0e96bfddef060d
Bub96 <loganeck197@gmail.com>
2024-08-19 21:58:53 -0500

    added def to print a book

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    book_path = 'books/frankenstein.txt'
+    txt = get_book_text(book_path)
+    print(txt)
+    print(num_of_words(txt))
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+
+
+
+main()
